7566f9bc0903f3165420aa4e687219d5b63d3d26
ganeshnanda32-sys <ganeshnanda32@gmail.com>
2026-02-07 09:36:22 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
-import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./config/db.js";
-import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 
-connectDB();
+const startServer = async () => {
+  await connectDB();
 
-app.use(express.json());
-app.use("/api/auth", authRoutes);
+  app.use(express.json());
+  app.use("/api/auth", authRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
-});
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
